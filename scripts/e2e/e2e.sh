aa397ffa97cf0dc276ac7a672e9aa2d8371b9045
Ivan Ivic <tyrion85@gmail.com>
2022-02-11 12:54:52 +0100

    fix paths

--- scripts/e2e/e2e.sh
+++ scripts/e2e/e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sh ./test_create_new_topic.sh
-sh ./test_add_new_partitions.sh
-sh ./test_change_cleanup_policy.sh
-sh ./test_delete_topic_manifest.sh
+sh ./scripts/e2/test_create_new_topic.sh
+sh ./scripts/e2/test_add_new_partitions.sh
+sh ./scripts/e2/test_change_cleanup_policy.sh
+sh ./scripts/e2/test_delete_topic_manifest.sh
